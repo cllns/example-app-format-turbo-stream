7514134f6c43ae3817d8ec3d3f04f5eb683b01d3
Sean Collins <sean@cllns.com>
2024-05-29 10:41:16 -0600

    Generate home.index action

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@
 module Bookshelf
   class Routes < Hanami::Routes
     # Add your routes here. See https://guides.hanamirb.org/routing/overview/ for details.
+    get "/home", to: "home.index"
   end
 end
